13922e98f1dafb1b69054726efe78eb67b768c64
Alam_Munquasim2 <Alam_Munquasim@epam.com>
2023-04-08 23:47:51 +0530

    working code

---
+++ spring-security-client/src/main/java/com/muka/client/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,11 @@
+package com.muka.client.repository;
+
+import com.muka.client.entity.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PasswordResetTokenRepository extends
+        JpaRepository<PasswordResetToken,Long> {
+    PasswordResetToken findByToken(String token);
+}
---
+++ spring-security-client/src/main/java/com/muka/client/service/UserService.java
@@ -0,0 +1,29 @@
+package com.muka.client.service;
+
+import com.muka.client.entity.User;
+import com.muka.client.entity.VerificationToken;
+import com.muka.client.model.UserModel;
+
+import java.util.Optional;
+
+public interface UserService {
+    User registerUser(UserModel userModel);
+
+    void saveVerificationTokenForUser(String token, User user);
+
+    String validateVerificationToken(String token);
+
+    VerificationToken generateNewVerificationToken(String oldToken);
+
+    User findUserByEmail(String email);
+
+    void createPasswordResetTokenForUser(User user, String token);
+
+    String validatePasswordResetToken(String token);
+
+    Optional<User> getUserByPasswordResetToken(String token);
+
+    void changePassword(User user, String newPassword);
+
+    boolean checkIfValidOldPassword(User user, String oldPassword);
+}
